0e4fc05fb0ecdf9df5883aa57a28223d5d33daf2
ayush1999-dot <ayushshettyc603@gmail.com>
2024-09-06 16:42:09 +0200

    second commit

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -12,5 +13,14 @@ func main() {
 	router.GET("/", func(extc *gin.Context) {
 		extc.JSON(http.StatusOK, "ayush here")
 	})
+
+	// Azure App Service sets the port as an Environment Variable
+	// This can be random, so needs to be loaded at startup
+	port := os.Getenv("HTTP_PLATFORM_PORT")
+
+	// default back to 8080 for local dev
+	if port == "" {
+		port = "8080"
+	}
 	router.Run("127.0.0.1:8080")
 }
