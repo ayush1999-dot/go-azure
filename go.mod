fe9aadd17c31ec05898215f38ea99d0f9137e6fd
ayush1999-dot <ayushshettyc603@gmail.com>
2024-09-08 19:09:26 +0200

    added html and watcher

--- go.mod
+++ go.mod
@@ -30,5 +30,6 @@ require (
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,23 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/fsnotify.v1"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 
 	router := gin.Default()
 
-	router.GET("/", func(extc *gin.Context) {
-		extc.JSON(http.StatusOK, "ayush here")
+	//router.GET("/", func(extc *gin.Context) {
+	//	extc.JSON(http.StatusOK, "ayush here")
+	//})
+	router.GET("/", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Hello from Go and Gin running on Azure App Service",
+		})
 	})
-
 	// Azure App Service sets the port as an Environment Variable
 	// This can be random, so needs to be loaded at startup
 	port := os.Getenv("HTTP_PLATFORM_PORT")
 
+	// creates a new file watcher for App_offline.htm
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println("ERROR", err)
+	}
+	defer watcher.Close()
+	// watch for App_offline.htm and exit the program if present
+	// This allows continuous deployment on App Service as the .exe will not be
+	// terminated otherwise
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if strings.HasSuffix(event.Name, "app_offline.htm") {
+					fmt.Println("Exiting due to app_offline.htm being present")
+					os.Exit(0)
+				}
+			}
+		}
+	}()
 	// default back to 8080 for local dev
 	if port == "" {
 		port = "8080"
